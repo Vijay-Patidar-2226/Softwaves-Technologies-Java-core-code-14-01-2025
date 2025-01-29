3df50f3829c6cca06c36f347a961f2f3167d3438
Vijay-Patidar-2226 <vijayharniya6267@gmail.com>
2025-01-29 08:51:45 -0800

    Add files via upload

---
+++ Test2_2.java
@@ -0,0 +1,21 @@
+class Test2_2
+{
+static public void main(String ar[])
+{
+for(int i=11;i==11;i+=0.5)
+{
+
+System.out.printf("%d%n",i);
+
+}
+
+
+}
+
+}
+
+// o/p=>11
+//      11  
+//      11
+//      11  
+//     infinite
