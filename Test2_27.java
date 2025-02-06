518a1b22ae80794551d12ee0cee6e527542290b3
Vijay Patidar <vijayharniya6267@gmail.com>
2025-02-06 01:01:22 -0800

    Add files via upload

---
+++ Test2_27.java
@@ -0,0 +1,29 @@
+class Test2_27
+{
+static public void main(String ar[])
+{
+
+
+int i=1;
+
+for(;i++<=5;)
+System.out.println("softwaves_1");
+
+System.out.println("softwaves_2");
+
+}
+}
+
+
+/*
+o/p=>
+
+     softwaves_1
+     softwaves_1
+     softwaves_1
+     softwaves_1
+     softwaves_1
+     softwaves_2
+     
+
+*/
